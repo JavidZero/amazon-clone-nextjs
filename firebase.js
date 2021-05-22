ded38d965e2648dc4a1d5989876bb76dfb594679
JavidZero <javid.zero@gmail.com>
2021-05-22 18:22:40 +0400

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCEZ_ZU4WuVGEDrvY7-IY9EosojGdQqJmE",
+  authDomain: "clone-b79ca.firebaseapp.com",
+  projectId: "clone-b79ca",
+  storageBucket: "clone-b79ca.appspot.com",
+  messagingSenderId: "742290563495",
+  appId: "1:742290563495:web:22f31202292c0cb72bd682",
+};
